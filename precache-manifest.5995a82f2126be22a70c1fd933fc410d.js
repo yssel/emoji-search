71db0b362ce69e2926eb12798e5cc55739083d96
yssel <yssel@users.noreply.github.com>
2023-09-14 03:47:53 +0000

    deploy: 7cdc44285c0a0a6c6d3df3bc41ec10a6616ff123

---
+++ precache-manifest.5995a82f2126be22a70c1fd933fc410d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84830b83bd6d1a90a081778a07a3bbd6",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "8cff1dcfdab618394e88",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/emoji-search/static/js/2.120a0f45.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search/static/js/2.120a0f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cff1dcfdab618394e88",
+    "url": "/emoji-search/static/js/main.e6612477.chunk.js"
+  },
+  {
+    "revision": "53f65d824d8bd704bdf5",
+    "url": "/emoji-search/static/js/runtime-main.fcefcbdb.js"
+  }
+]);
